33db728d77c32a431645e42dde061f7b2ee09032
chihebgit <chiheb.samti@gmail.com>
2023-06-20 00:01:48 +0200

    bug fix : query params

--- src/Container/BeerList.jsx
+++ src/Container/BeerList.jsx
@@ -7,8 +7,8 @@ import FilterComponent from "../components/Filter";
 import { useEffect } from "react";
 
 const BeerList = () => {
-  const [searchParam, setSearchParam] = useState("");
-  const [filtreParam, setFiltreParam] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filtreQuery, setFiltreQuery] = useState("");
   const [caching, setCaching] = useState(true);
 
   // Call the useBeers hook to fetch and manage beer data
@@ -20,25 +20,25 @@ const BeerList = () => {
     handlePrevPage,
     handleNextPage,
     handlePageChange,
-  } = useBeers(caching, filtreParam, searchParam);
+  } = useBeers(caching, filtreQuery, searchQuery);
 
   // Reset the search and filter parameters
   const handleReset = () => {
     setCaching(true);
-    setSearchParam("");
-    setFiltreParam("");
+    setSearchQuery("");
+    setFiltreQuery("");
   };
 
   // Update the filter parameter and disable caching
   const handleFilter = (query) => {
-    setFiltreParam(query);
+    setFiltreQuery(query);
     setCaching(false);
   };
 
   // Update the search parameter and disable caching
   const handleSearch = (query) => {
     setCaching(false);
-    setSearchParam(query);
+    setSearchQuery(query);
   };
 
   return (
--- src/hook/useBeers.jsx
+++ src/hook/useBeers.jsx
@@ -2,49 +2,39 @@ import { useState, useEffect } from "react";
 import ApiHandler from "../utils/ApiHandler";
 
 const useBeer = (isCaching = true, filtreQuery = "", searchQuery = "") => {
-  //set use states
   const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   const [lastPage, setLastPage] = useState(null);
 
-  // set the query string
   let query;
-  filtreQuery ? (query = "&" + filtreQuery) : "&";
-  searchQuery ? (query += "&" + searchQuery) : "";
+  filtreQuery ? (query = "&" + filtreQuery) : (query = "");
+  searchQuery ? (query += "&" + "beer_name="+searchQuery) : "";
 
-  // fetch data
   useEffect(() => {
-    fetchData(currentPage);
-  }, [currentPage, filtreQuery, searchQuery, query]);
+    fetchData(currentPage, query);
+  }, [currentPage, query]);
 
-  // fetch data function
-  const fetchData = async (page) => {
+  const fetchData = async (page, query) => {
     setIsLoading(true);
-    // config session storage variables
     const lastPageKey = isCaching ? "last_page" : "last_query_page";
     const cacheKey = `beers_${page}`;
     const cachedData = sessionStorage.getItem(cacheKey);
 
-    // fetch data from session storage
     if (cachedData) {
       setData(JSON.parse(cachedData));
       setIsLoading(false);
     } else {
-      // fetch data from ApiHandler
       try {
         const apiHandler = new ApiHandler();
         const fetchedData = await apiHandler.getBeers(page, query);
-        console.log(page);
         setData(fetchedData);
 
-        // set data to session storage
         if (isCaching) {
           sessionStorage.setItem(cacheKey, JSON.stringify(fetchedData));
         }
 
-        // store last page to local storage
-        if (fetchedData.length == 20) {
+        if (fetchedData.length === 20) {
           sessionStorage.setItem(lastPageKey, JSON.stringify(page + 1));
         }
       } catch (error) {
@@ -52,12 +42,9 @@ const useBeer = (isCaching = true, filtreQuery = "", searchQuery = "") => {
       }
     }
 
-    //set last page
     setLastPage(sessionStorage.getItem(lastPageKey));
     setIsLoading(false);
   };
-
-  // handle pagination actions
 
   const handlePrevPage = () => {
     if (currentPage > 1) setCurrentPage(currentPage - 1);
@@ -71,7 +58,6 @@ const useBeer = (isCaching = true, filtreQuery = "", searchQuery = "") => {
     setCurrentPage(page);
   };
 
-  //return props
   return {
     data,
     currentPage,
